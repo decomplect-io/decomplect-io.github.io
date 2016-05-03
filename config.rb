8680b8473f1362f27c8b3a570bfafb50ac24e1bd
Mohit Thatte <mohit.thatte@gmail.com>
2016-05-03 20:20:33 +0530

    Start from scratch!
    - latest middleman, new site begins here.

--- config.rb
+++ config.rb
@@ -1,24 +1,44 @@
-set :css_dir, 'stylesheets'
+###
+# Page options, layouts, aliases and proxies
+###
 
-set :js_dir, 'javascripts'
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
 
-set :images_dir, 'images'
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
 
-activate :automatic_image_sizes
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
+# General configuration
+
+# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-configure :build do
-  activate :minify_css
-  activate :minify_javascript
-  activate :asset_hash
-  activate :relative_assets
-end
+###
+# Helpers
+###
 
-activate :deploy do |deploy|
-  deploy.method       = :git
-  deploy.branch       = 'master'
-  deploy.build_before = true
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
 end
